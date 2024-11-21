b40b7e4bcfe58c396ddf01c329f3d3d127bcdf22
Marcelo Costa da Mata <mcosta.matta@gmail.com>
2024-11-21 16:47:15 -0300

    Conclusão do menu de opçoes, e ajuste

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Desenvolvimento dos métodos Aplicar CDB e Aplicar LCI, e suas lógicas de cálc
 Ajuste da lógica (utilização da fórmula dos juros compostos) a ser implementada nos dois casos;
 Conclusão da classe Aplica Banco, com sua lógica, menu de opções e métodos;
 Desenvolvimento do Menu de opções para acesso aos serviços de conta, ou investimentos;
+Conclusão do Menu de opções na classe Gerencia Banco. Serão feitos testes para verificar se o programa funciona corretamente;
--- src/main/java/com/gerenciabanco2/GerenciaBanco2.java
+++ src/main/java/com/gerenciabanco2/GerenciaBanco2.java
@@ -5,52 +5,55 @@ import java.util.Scanner;
 public class GerenciaBanco2 {
     static Scanner input = new Scanner(System.in);
 
-    public void MenuOpcoes() {
-        try (Scanner input = new Scanner(System.in)) {
-            int opcao;
-
-            do {
-                System.out.println("----------Seja Bem Vindo!----------");
-                System.out.println("-----Selecione a opção desejada----");
-                System.out.println("|----1 - Transações comuns--------|");
-                System.out.println("|----2 - Investimentos------------|");
-                System.out.println("|----3 - Sair---------------------|");
-
-                opcao = input.nextInt();
+    public static Cliente ObterDadosClientes() {
+            System.out.println("Informe o nome do Cliente: ");
+            String nomeCliente = input.nextLine();
+    
+            System.out.println("Informe o sobrenome do Cliente: ");
+            String sobrenomeCliente = input.nextLine();
+    
+            System.out.println("Informe o CPF do Cliente: ");
+            String cpfCliente = input.nextLine();
+    
+            return new Cliente(nomeCliente, sobrenomeCliente, cpfCliente);
+        }
+    
+        public static void MenuOpcoes() {
+            try (Scanner input = new Scanner(System.in)) {
+                int opcao;
+    
+                do {
+                    System.out.println("----------Seja Bem Vindo!----------");
+                    System.out.println("-----Selecione a opção desejada----");
+                    System.out.println("|----1 - Transações comuns--------|");
+                    System.out.println("|----2 - Investimentos------------|");
+    
+                    opcao = input.nextInt();
+                    input.nextLine();
+    
+                    Cliente cliente = ObterDadosClientes();
 
                 switch (opcao) {
                     case 1:
-                        System.out.println("Informe o nome do Cliente: ");
-                        String nomeCliente = input.nextLine();
-
-                        System.out.println("Informe o sobrenome do Cliente: ");
-                        String sobrenomeCliente = input.nextLine();
-
-                        System.out.println("Informe o CPF do Cliente: ");
-                        String cpfCliente = input.nextLine();
-
-                        Cliente cliente = new Cliente(nomeCliente, sobrenomeCliente, cpfCliente);
                         ContaBanco conta = new ContaBanco(cliente);
-
                         System.out.println(conta.toString());
                         conta.opcoes();
                         break;
                     case 2:
-
-                        break;
-                    case 3:
-
+                        AplicaBanco investimento = new AplicaBanco(cliente);
+                        System.out.println(cliente.toString());
+                        investimento.opcoesInvest();
                         break;
                     default:
-                        break;
+                        System.out.println("Opção inválida! Tente novamente");
+                        MenuOpcoes();
                 }
-
-            } while (opcao != 3);
+            } while (opcao != 2);
             System.exit(0);
         }
     }
 
     public static void main(String[] args) {
-        
+        MenuOpcoes();
     }
 }
